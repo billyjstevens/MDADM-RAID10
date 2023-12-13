177f330ad91b96f499172583e1769e278b995f09
Billy Stevens <bste@sonway.org>
2023-12-12 22:40:16 -0600

    added de-raid script

---
+++ remove-raid10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# RAID device to dismantle
+RAID_DEVICE="/dev/md0" # ATTENTION you must modify this for each RAID partition ie. md0, md1, md2
+# Drives that were part of the RAID array
+DRIVES="/dev/sda /dev/sdb /dev/sdc /dev/sdd"
+# Unmount the RAID array if mounted
+umount ${RAID_DEVICE}* || echo "No mounts found for ${RAID_DEVICE}"
+# Stop the RAID array
+mdadm --stop ${RAID_DEVICE}
+# Remove RAID superblock from each drive
+for drive in ${DRIVES}; do
+mdadm --zero-superblock ${drive}
+done
+echo "RAID10 array ${RAID_DEVICE} has been dismantled. Drives are now separate."
